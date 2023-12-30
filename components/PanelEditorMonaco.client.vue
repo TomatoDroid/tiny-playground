e62adefbad84cb99fcc2dd49f6baac9fb1c61f40
zhen <770226915@qq.com>
2023-12-30 18:32:15 +0800

    feat: Disable saving with Ctrl/Cmd + S in Monaco Editor.

--- components/PanelEditorMonaco.client.vue
+++ components/PanelEditorMonaco.client.vue
@@ -102,6 +102,8 @@ watch(
       emit('change', value)
     })
 
+    editor.addCommand(monaco.KeyMod.CtrlCmd | monaco.KeyCode.KeyS, () => {})
+
     watch(
       () => props.filepath,
       () => {
